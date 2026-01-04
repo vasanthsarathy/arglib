88e39a24ca9f29c392d8664ebce2eb3b862dc010
Vasanth Sarathy <vsarathy@gmail.com>
2026-01-04 11:20:10 -0500

    Bump version to 0.1.9

--- arglib/__init__.py
+++ arglib/__init__.py
@@ -21,4 +21,4 @@ __all__ = [
     "__version__",
 ]
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,4 +2,4 @@ import arglib
 
 
 def test_version():
-    assert arglib.__version__ == "0.1.8"
+    assert arglib.__version__ == "0.1.9"
